98952c5ee89701dcacf4aaacd0e909acb0d3def5
Isha <isha18919@gmail.com>
2025-06-01 20:51:56 +0530

    updating assignment 4

--- Assignment-4/assignment 4/src/components/Showcase.jsx
+++ Assignment-4/assignment 4/src/components/Showcase.jsx
@@ -10,8 +10,9 @@ const CardBox = ({ title, children }) => (
     borderRadius: '12px',
     padding: '20px',
     marginBottom: '25px',
-    backgroundColor: 'rgb(197, 225, 235)',
+    backgroundColor: '#DAC8BC',
     boxShadow: '0 2px 6px rgba(0,0,0,0.05)'
+
   }}>
     <h2 style={{ marginBottom: '15px', color: '#333' }}>{title}</h2>
     <div>{children}</div>
@@ -23,30 +24,30 @@ const Showcase = () => {
     <div style={{ 
       padding: '40px', 
       fontFamily: 'Segoe UI, sans-serif', 
-      backgroundColor: '#ffffff', 
+      backgroundColor: ' #2D323F', 
       minHeight: '100vh' 
     }}>
-      <h1 style={{ textAlign: 'center', marginBottom: '40px' }}>
+      <h1 style={{ textAlign: 'center', marginBottom: '40px', color:' #ffffff' }}>
          Showcase
       </h1>
 
-      <div style={{ display: 'grid', gap: '30px', gridTemplateColumns: '1fr 1fr' }}>
-        <CardBox title="🧩 UserBadge Examples">
+      <div style={{ display: 'grid', gap: '30px', gridTemplateColumns: '1fr 1fr', color: 'rgb(45, 50, 63)' }}>
+        <CardBox title="UserBadge Examples">
           <UserBadge name="Isha" role="Admin" isVerified={true} />
           <UserBadge name="Tisha" role="User" isVerified={false} />
         </CardBox>
 
-        <CardBox title="🔁 Status Toggle">
+        <CardBox title=" Status Toggle">
           <StatusToggle />
         </CardBox>
 
-        <CardBox title="📋 Task List Manager">
+        <CardBox title=" Task List Manager">
           <TaskList />
         </CardBox>
 
-        <CardBox title="👤 Profile Viewer Variants">
+        <CardBox title=" Profile Viewer ">
           <ProfileViewer name="Isha" email="isha18919@gmail.com" isPremiumUser={true} />
-          <ProfileViewer name="Esha" email="23amtics@gmail.com" isPremiumUser={false} />
+          <ProfileViewer name="Isha" email="23amtics@gmail.com" isPremiumUser={false} />
         </CardBox>
       </div>
     </div>
