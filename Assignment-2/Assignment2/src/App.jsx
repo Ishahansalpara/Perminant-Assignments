4b8a8bf57560fe84e386c2734666571efa3722a3
Isha <isha18919@gmail.com>
2025-06-01 16:40:41 +0530

    updating assignment 2

---
+++ Assignment-2/Assignment2/src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import Toggle from './components/Toggle';
+import Form from './components/Form';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <header>
+        <h1>Assignment 2</h1>
+    
+      <Toggle />
+      <Form />
+      </header>
+    </div>
+  );
+}
+
+export default App;
